40effce39bc9dfabe001a30728a7ea8e0f61c585
immanoj16 <mmaharana14@gmail.com>
2017-11-29 21:37:07 +0530

    Add: addingredient handler

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -6,14 +6,17 @@ import BuildControl from './BuildControl/BuildControl';
 const controls = [
   {label: 'Salad', type: 'salad'},
   {label: 'Bacon', type: 'bacon'},
-  {label: 'Cheese', type: 'Cheese'},
+  {label: 'Cheese', type: 'cheese'},
   {label: 'Meat', type: 'meat'}
 ];
 
 const BuildControls = (props) => (
   <div className={classes.BuildControls}>
     {controls.map(control => {
-      return <BuildControl key={control.label} label={control.label} />
+      return <BuildControl
+                key={control.label}
+                label={control.label}
+                added={() => props.ingredientAdded(control.type)} />
     })}
   </div>
 )
