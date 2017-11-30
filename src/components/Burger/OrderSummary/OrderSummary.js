3546585a748cc2307850c2e5ec16d172a1ac5af8
immanoj16 <mmaharana14@gmail.com>
2017-11-30 10:46:28 +0530

    Add: backdrop for the modal

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import Aux from '../../../hoc/Aux';
+import { Ingredient, Close } from './OrderSummary.css';
 
 const OrderSummary = (props) => {
 
@@ -8,13 +9,14 @@ const OrderSummary = (props) => {
     .map(igKey => {
       return (
         <li key={igKey}>
-          <span>{igKey}</span>: {props.ingredients[igKey]}
+          <span className={Ingredient}>{igKey}</span>: {props.ingredients[igKey]}
         </li>
       );
     })
 
   return (
     <Aux>
+      <span className={Close} onClick={props.modalClosed}>X</span>
       <h3>Your Order!</h3>
       <p>A delicious berger with the following ingredients:</p>
       <ul>
