e041f2a9e2defd4b50aa85ef777947d4f832c1d9
immanoj16 <mmaharana14@gmail.com>
2017-12-02 18:56:52 +0530

    Add: purchasehandler

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -5,6 +5,7 @@ import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
+import axios from '../../axios-orders';
 
 const INGREDIENT_PRICES = {
   salad: 0.5,
@@ -86,7 +87,23 @@ class BurgerBuilder extends Component {
   }
 
   purchaseContinueHandler = () => {
-    alert('You continue!!');
+    const order = {
+      ingredients: this.state.ingredients,
+      price: this.state.totalPrice,
+      customer: {
+        name: 'Manoj Kumar',
+        email: 'mmaharana14@gmail.com',
+        address: {
+          city: 'Bhubaneswar',
+          pin: '75003',
+          country: 'India'
+        }
+      },
+      deliveryMethod: 'fastest'
+    }
+    axios.post('/orders.json', order)
+      .then(response => console.log(response))
+      .catch(error => console.log(error))
     this.purchaseCancelHandler();
   }
 
