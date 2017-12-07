ee91500e3e285aa5b1b9325b5b6ae488d71f76f0
immanoj16 <mmaharana14@gmail.com>
2017-12-07 18:59:09 +0530

    Add: handler methods to the checkout buttons

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -89,28 +89,29 @@ class BurgerBuilder extends Component {
   }
 
   purchaseContinueHandler = () => {
-    this.setState({loading: true});
-    const order = {
-      ingredients: this.state.ingredients,
-      price: this.state.totalPrice,
-      customer: {
-        name: 'Manoj Kumar',
-        email: 'mmaharana14@gmail.com',
-        address: {
-          city: 'Bhubaneswar',
-          pin: '751003',
-          country: 'India'
-        }
-      },
-      deliveryMethod: 'fastest'
-    }
-    axios.post('/orders.json', order)
-      .then(response => {
-        this.setState({loading: false, purchasing: false})
-      })
-      .catch(error => {
-        this.setState({loading: false, purchasing: false})
-      })
+    // this.setState({loading: true});
+    // const order = {
+    //   ingredients: this.state.ingredients,
+    //   price: this.state.totalPrice,
+    //   customer: {
+    //     name: 'Manoj Kumar',
+    //     email: 'mmaharana14@gmail.com',
+    //     address: {
+    //       city: 'Bhubaneswar',
+    //       pin: '751003',
+    //       country: 'India'
+    //     }
+    //   },
+    //   deliveryMethod: 'fastest'
+    // }
+    // axios.post('/orders.json', order)
+    //   .then(response => {
+    //     this.setState({loading: false, purchasing: false})
+    //   })
+    //   .catch(error => {
+    //     this.setState({loading: false, purchasing: false})
+    //   })
+    this.props.history.push('/checkout');
   }
 
   render () {
