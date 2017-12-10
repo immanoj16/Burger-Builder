05fb64c0125a709b1721add286c1f27a87abb8a6
immanoj16 <mmaharana14@gmail.com>
2017-12-10 10:29:42 +0530

    Fetch orders from database

--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -1,8 +1,32 @@
 import React, { Component } from 'react';
 
 import Order from '../../components/Order/Order';
+import axios from '../../axios-orders';
 
 class Orders extends Component {
+
+  state = {
+    orders: [],
+    loading: true
+  }
+
+  componentDidMount () {
+    const fetchedOrders = [];
+    axios.get('orders.json')
+      .then(response => {
+        for (let key in response.data) {
+          fetchedOrders.push({
+            ...response.data[key],
+            id: key
+          })
+        }
+        this.setState({loading: false, orders: fetchedOrders});
+      })
+      .catch(error => {
+        this.setState({loading: false})
+      })
+  }
+
   render () {
     return (
       <div>
