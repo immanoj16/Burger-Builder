a144095c2f89a200061858117a2876ccd7766d5d
immanoj16 <mmaharana14@gmail.com>
2017-12-10 09:36:03 +0530

    Add: order button with its working in contactdata

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -7,22 +7,23 @@ import ContactData from './ContactData/ContactData';
 class Checkout extends Component {
 
   state = {
-    ingredients: {
-      salad: 0,
-      bacon: 0,
-      cheese: 0,
-      meat: 0
-    }
+    ingredients: null,
+    price: 0
   }
 
-  componentDidMount () {
+  componentWillMount () {
     const query = new URLSearchParams(this.props.location.search);
     const ingredients = {};
+    let price = 0;
     for (let param of query.entries()) {
       // param = ['salad', '1']
-      ingredients[param[0]] = +param[1];
+      if (param[0] === 'price') {
+        price = param[1];
+      } else {
+        ingredients[param[0]] = +param[1];
+      }
     }
-    this.setState({ingredients});
+    this.setState({ingredients, price});
   }
 
   checkoutCancelledHandler = () => {
@@ -40,7 +41,9 @@ class Checkout extends Component {
           ingredients={this.state.ingredients}
           checkoutCancelled={this.checkoutCancelledHandler}
           checkoutContinued={this.checkoutContinuedHandler} />
-        <Route path={this.props.match.path + '/contact-data'} component={ContactData} />
+        <Route
+          path={this.props.match.path + '/contact-data'}
+          render={(props) => (<ContactData ingredients={this.state.ingredients} totalPrice={this.state.price} {...props} />)} />
       </div>
     );
   }
