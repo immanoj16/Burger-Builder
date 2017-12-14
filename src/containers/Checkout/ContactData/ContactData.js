762b38d1fa14aca907770c44475e254a8a0fedd8
immanoj16 <mmaharana14@gmail.com>
2017-12-14 10:04:35 +0530

    Add: input changed handler to each element

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -82,8 +82,18 @@ class ContactData extends Component {
       })
   }
 
-  inputChangedHandler = (event) => {
-    console.log(event.target.value);
+  inputChangedHandler = (event, inputIdentifier) => {
+    const updatedOrderForm = {
+      ...this.state.orderForm
+    };
+
+    const updatedFormElement = {
+      ...updatedOrderForm[inputIdentifier]
+    };
+
+    updatedFormElement.value = event.target.value;
+    updatedOrderForm[inputIdentifier] = updatedFormElement;
+    this.setState({orderForm: updatedOrderForm});
   }
 
   render () {
@@ -105,7 +115,7 @@ class ContactData extends Component {
             elementType={formElement.config.elementType}
             elementConfig={formElement.config.elementConfig}
             value={formElement.config.value}
-            changed={this.inputChangedHandler} />
+            changed={(event) => this.inputChangedHandler(event, formElement.id)} />
         ))}
         <Button btnType='Success' clicked={this.orderHandler}>Order</Button>
       </form>
