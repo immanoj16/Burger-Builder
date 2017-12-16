8567f54b99233f7019da0b3b631459db90e76047
immanoj16 <mmaharana14@gmail.com>
2017-12-16 13:36:12 +0530

    Add: invalid css classes with valid props

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -16,7 +16,11 @@ class ContactData extends Component {
           type: 'text',
           placeholder: 'Your Name'
         },
-        value: ''
+        value: '',
+        validation: {
+          required: true
+        },
+        valid: false
       },
       email: {
         elementType: 'input',
@@ -24,7 +28,11 @@ class ContactData extends Component {
           type: 'email',
           placeholder: 'Your E-mail'
         },
-        value: ''
+        value: '',
+        validation: {
+          required: true
+        },
+        valid: false
       },
       city: {
         elementType: 'input',
@@ -32,7 +40,11 @@ class ContactData extends Component {
           type: 'text',
           placeholder: 'Your City'
         },
-        value: ''
+        value: '',
+        validation: {
+          required: true
+        },
+        valid: false
       },
       pin: {
         elementType: 'input',
@@ -40,7 +52,12 @@ class ContactData extends Component {
           type: 'text',
           placeholder: 'ZIP code'
         },
-        value: ''
+        value: '',
+        validation: {
+          required: true,
+          len: 6
+        },
+        valid: false
       },
       country: {
         elementType: 'input',
@@ -48,7 +65,11 @@ class ContactData extends Component {
           type: 'text',
           placeholder: 'Your Country'
         },
-        value: ''
+        value: '',
+        validation: {
+          required: true
+        },
+        valid: false
       },
       deliveryMethod: {
         elementType: 'select',
@@ -87,6 +108,20 @@ class ContactData extends Component {
       })
   }
 
+  checkValidity(value, rules) {
+    let isValid = true;
+
+    if (rules.required) {
+      isValid = value.trim() !== '' && isValid;
+    }
+
+    if(rules.len) {
+      isValid = value.length === rules.len && isValid;
+    }
+
+    return isValid;
+  }
+
   inputChangedHandler = (event, inputIdentifier) => {
     const updatedOrderForm = {
       ...this.state.orderForm
@@ -97,7 +132,9 @@ class ContactData extends Component {
     };
 
     updatedFormElement.value = event.target.value;
+    updatedFormElement.valid = this.checkValidity(updatedFormElement.value, updatedFormElement.validation)
     updatedOrderForm[inputIdentifier] = updatedFormElement;
+    console.log(updatedFormElement);
     this.setState({orderForm: updatedOrderForm});
   }
 
@@ -112,6 +149,8 @@ class ContactData extends Component {
       })
     }
 
+    console.log(formElements);
+
     let form = (
       <form onSubmit={this.orderHandler}>
         {formElements.map(formElement => (
@@ -120,6 +159,7 @@ class ContactData extends Component {
             elementType={formElement.config.elementType}
             elementConfig={formElement.config.elementConfig}
             value={formElement.config.value}
+            invalid={!formElement.config.valid}
             changed={(event) => this.inputChangedHandler(event, formElement.id)} />
         ))}
         <Button btnType='Success' clicked={this.orderHandler}>Order</Button>
